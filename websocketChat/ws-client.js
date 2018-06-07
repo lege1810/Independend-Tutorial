160f096edf40a18a4b155204dc15f2fc9ac86500
nordie92 <m.maart@gmx.de>
2018-06-07 11:32:19 +0200

    node.js chat (noch ohne mongo)
    Leider sind install-files dabei, shit happens

--- websocketChat/ws-client.js
+++ websocketChat/ws-client.js
@@ -1,38 +1,45 @@
-window.addEventListener("load", function () {
+window.addEventListener("load", function() {
     // if user is running mozilla then use it's built-in WebSocket
     window.WebSocket = window.WebSocket || window.MozWebSocket;
-  
+
     var connection = new WebSocket('ws://127.0.0.1:1337');
-  
-    connection.onopen = function () {
-      // connection is opened and ready to use
-      document.getElementById("wsConnection").textContent = "Connected"
-    };
-  
-    connection.onerror = function (error) {
-      // an error occurred when sending/receiving data
-      document.getElementById("wsConnection").textContent = "Error"
-    };
-  
-    connection.onmessage = function (message) {
-      // try to decode json (I assume that each message
-      // from server is json)
-      try {
-        var json = JSON.parse(message.data);
-      } catch (e) {
-        console.log('This doesn\'t look like a valid JSON: ',
-            message.data);
-        return;
-      }
-      // handle incoming message
-      var wsChatContent = document.getElementById("wsChatContent")
-      wsChatContent.innerHTML = wsChatContent.innerHTML + "<br>" + message.data
 
+    connection.onopen = function() {
+        console.log('Connected');
     };
 
-    document.getElementById("wsSendTextButton").addEventListener("click", function(){
-        var texttoSend = document.getElementById("wsText")
-        connection.send(texttoSend.value)
-        texttoSend.value = ""
+    connection.onerror = function(error) {
+        console.log('Error');
+    };
+
+    connection.onmessage = function(message) {
+        try {
+            var json = JSON.parse(JSON.parse(message.data));
+            if (getTutorium() == json.tutorium) {
+                var content = document.getElementById("content");
+                content.innerHTML = content.innerHTML + "<br>" + json['name'] + ': ' + json['message'];
+            }
+        } catch (e) {
+            console.log('This doesn\'t look like a valid JSON: ',
+                message.data);
+            return;
+        }
+    };
+
+    document.getElementById("send").addEventListener("click", function() {
+        var message = document.getElementById("message");
+        var name = document.getElementById("name");
+        message = {
+            'tutorium': getTutorium(),
+            'message': message.value,
+            'name': name.value
+        }
+        connection.send(JSON.stringify(message));
+        message.value = "";
     });
-  });
+});
+
+function getTutorium() {
+    var tutorium = document.getElementById("tutorium");
+    return tutorium.options[tutorium.selectedIndex].value;
+}
--- websocketChat/ws.js
+++ websocketChat/ws.js
@@ -4,34 +4,33 @@ var WebSocketServer = require('websocket').server;
 var http = require('http');
 var clients = []
 var server = http.createServer(function(request, response) {
-  // process HTTP request. Since we're writing just WebSockets
-  // server we don't have to implement anything.
+    // process HTTP request. Since we're writing just WebSockets
+    // server we don't have to implement anything.
 });
-server.listen(1337, function() { });
+server.listen(1337, function() {});
 
 // create the server
 wsServer = new WebSocketServer({
-  httpServer: server
+    httpServer: server
 });
 
 // WebSocket server
 wsServer.on('request', function(request) {
-  var connection = request.accept(null, request.origin);
-  clients.push(connection) 
-  // This is the most important callback for us, we'll handle
-  // all messages from users here.
-  connection.on('message', function(message) {
-    if (message.type === 'utf8') {
-      // process WebSocket message
-      console.log("Nachricht : " + JSON.stringify(message.utf8Data))
-      for(var i = 0; i < clients.length; i++){
-        clients[i].send(JSON.stringify(message.utf8Data))
-      }
-      
-    }
-  });
+    var connection = request.accept(null, request.origin);
+    clients.push(connection)
+        // This is the most important callback for us, we'll handle
+        // all messages from users here.
+    connection.on('message', function(message) {
+        if (message.type === 'utf8') {
+            // process WebSocket message
+            console.log("Nachricht : " + JSON.stringify(message.utf8Data))
+            for (var i = 0; i < clients.length; i++) {
+                clients[i].send(JSON.stringify(message.utf8Data))
+            }
+        }
+    });
 
-  connection.on('close', function(connection) {
-    // close user connection
-  });
+    connection.on('close', function(connection) {
+        // close user connection
+    });
 });
